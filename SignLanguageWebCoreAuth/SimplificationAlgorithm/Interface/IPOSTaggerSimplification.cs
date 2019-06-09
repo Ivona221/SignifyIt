d150e4bb57261b4152bcb489dc117162226d1e26
Ivona <ivonamilanova221@gmail.com>
2019-06-09 23:24:47 +0200

    Add pos tagger

--- SignLanguageWebCoreAuth/SimplificationAlgorithm/Interface/IPOSTaggerSimplification.cs
+++ SignLanguageWebCoreAuth/SimplificationAlgorithm/Interface/IPOSTaggerSimplification.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SignLanguageWebCoreAuth.SimplificationAlgorithm.Interface
 {
-    public interface IPOSTagger
+    public interface IPOSTaggerSimplification
     {
         Dictionary<string, string> PosTag(Dictionary<string, string> text);
     }
